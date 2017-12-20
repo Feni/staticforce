6f727e7f3b46fac088f9cc4ef47e41dc60d7437b
Feni Varughese <feni727@gmail.com>
2017-12-19 23:36:10 -0600

    Rendering for text and objects.

--- src/components/Statement.vue
+++ src/components/Statement.vue
@@ -1,14 +1,35 @@
 <template>
-    <div class="row DataRow">
-        <div class="four wide column">
+    <div class="row DataRow" v-on:click="select">
+        <div class="two wide column">
             <label class="DataLabel">{{ meta.name }}</label>
         </div>
         
         <div class="eight wide column">
-            <input type="text" v-model="data.value" class="DataInput"/>
+            <!-- TODO: Align right on numbers. Dynamic input type -->
+            <div class="edit" v-if="edit">
+                
+                <input type="text" v-model="data.value" class="DataInput"/>
+                <p v-if="datatype == 'equation'">{{ value }}</p>
+                
+            </div>
+            <div class="view" v-else>
             
-            
-            <p v-if="datatype == 'equation'">{{ value }}</p>
+                <div v-if="datatype == 'object'">
+                    
+                    <Statement v-for="statement in data.value" :key="statement.id"
+                       v-bind:id="statement.id"
+                       v-bind:meta="statement.meta"
+                       v-bind:datatype="statement.datatype"
+                       v-bind:data="statement.data"></Statement>
+
+                    
+                    
+                    
+                </div>
+                <div v-else>
+                    {{ data.value }}
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -17,11 +38,38 @@
 import Vue from "vue";
 
 export default Vue.extend({
+    name: 'Statement',
     props: ['id', 'meta', 'datatype', 'data'],
+    data: function() {
+        return {
+            edit: false
+        }
+    },
     computed: {
         value: function() {
             return this.$store.getters.getValue(this.id)
         }
+    },
+    methods: {
+        select: function(event) {
+            /*
+            console.log("Clicked on row");
+            console.log(this.id)
+            console.log(this.edit);
+            this.edit = true;
+            window.dashform.$emit('clear-row-edit');
+            let row = this;
+            let clearCallback = function(e){
+                console.log("Clearing row");
+                console.log(e);
+                row.edit = false;
+                // Remove this handler
+                window.dashform.removeEventListener('clear-row-edit', clearCallback);
+            }
+            window.dashform.$on('clear-row-edit', clearCallback)
+            */
+        }
+        
     }
 });
 </script>
